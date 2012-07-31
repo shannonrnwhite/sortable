615c54ac73370d5bead02c634f53c8fbbba74e1b
shannonrnwhite <shannon_r_white@hotmail.com>
2012-07-31 19:48:07 -0400

    Same as last

--- src/codingchallenge/ProductsIndex.java
+++ src/codingchallenge/ProductsIndex.java
@@ -1,27 +1,5 @@
 package codingchallenge;
 
-/*
-Copyright (c) 2012 Shannon White
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
-
 import java.util.*;
 
 /**
@@ -45,7 +23,11 @@ public class ProductsIndex implements TextIndex {
     public ProductsIndex(TextIndex modelIndex, TextIndex familyIndex, Set<Integer> noFamilyProducts) {
         this.modelIndex = modelIndex;
         this.familyIndex = familyIndex;
-        this.noFamilyProducts = noFamilyProducts;
+        if (noFamilyProducts == null) {
+            this.noFamilyProducts = Collections.emptySet();
+        } else {        	
+            this.noFamilyProducts = noFamilyProducts;
+        }
     }
 
     /**
@@ -53,8 +35,14 @@ public class ProductsIndex implements TextIndex {
      * Those products who have no family, but have a matching model, are also returned.
      */
     public Set<Integer> bestMatches(String query) {
-        Set<Integer> modelMatches = modelIndex.bestMatches(query);
-        Set<Integer> familyMatches = familyIndex.bestMatches(query);
+        Set<Integer> modelMatches = Collections.emptySet();
+        if (modelIndex != null) {
+        	modelMatches = modelIndex.bestMatches(query);
+        }
+        Set<Integer> familyMatches = Collections.emptySet();
+        if (familyIndex != null) {
+        	familyMatches = familyIndex.bestMatches(query);
+        }
         
         Set<Integer> intersection = new HashSet<Integer>();
         for (Integer match: modelMatches) {
